3c7694b11fddec7c42e0aaa5f72e4d4053ed54b1
SUPRIYA KUMAR MONDAL <sups149@gmail.com>
2024-04-24 23:32:44 +0200

    Adding refresh property configuration.

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ bootstraping from aws parameter store as a external property source.
 
 ## API
 http://localhost:8080/v1/customer/greeting/{name}
+
+## Environment variables
+`AWS_REGION=eu-west-1
+AWS_SHARED_CREDENTIALS_FILE=C:/User/Uid/.aws/credentials`
+
+_App will not work without AWS connection and parameter store created._
+## Reference
+https://docs.awspring.io/spring-cloud-aws/docs/3.1.0/reference/html/index.html#spring-cloud-aws-parameter-store
--- src/main/java/com/spnd/aws/ssm/controllers/CommonController.java
+++ src/main/java/com/spnd/aws/ssm/controllers/CommonController.java
@@ -1,12 +1,14 @@
 package com.spnd.aws.ssm.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 @RequestMapping("v1/customer")
 public class CommonController {
     @Value("${user.message}")
